b7e1763e1628e393d7bfbca60f8f340588bd2618
huseyinonalcom <65453275+huseyinonalcom@users.noreply.github.com>
2024-08-19 03:06:45 +0300

    s

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -4,45 +4,52 @@ import { NavLink } from "@/components/navigation/NavLink";
 import { useTranslations } from "next-intl";
 import { unstable_setRequestLocale } from "next-intl/server";
 import Image from "next/image";
+import { ReactNode } from "react";
 import { FaChartLine, FaCode, FaPaintBrush } from "react-icons/fa";
 
 type Props = {
   params: { locale: string };
 };
 
+const CardDiv = ({ children }: { children: ReactNode }) => {
+  return <div className="w-full max-w-[1400px] bg-white/90 backdrop-blur-md p-4 rounded-md">{children}</div>;
+};
+
 export default function Home({ params: { locale } }: Props) {
   unstable_setRequestLocale(locale);
   const t = useTranslations();
-  const maxWidth = "max-w-[1400px]";
+
   return (
     <div className="flex flex-col w-full gap-12 items-center bg-black p-12">
-      <div className={`w-full flex flex-row ${maxWidth} justify-around gap-5 bg-white/90 backdrop-blur-md p-4 rounded-md`}>
-        <div className="flex flex-col gap-2 items-center justify-center">
-          <h1 className="text-4xl font-bold">{t("intro-title")}</h1>
-          <p className="text-lg text-center">{t("intro-text")}</p>
-          <div className="flex flex-row gap-4">
-            <NavLink href="projects">
-              <ButtonColorOut>{t("intro-projects")}</ButtonColorOut>
-            </NavLink>
-            <NavLink href="contact">
-              <ButtonColorOut>{t("intro-contact")}</ButtonColorOut>
-            </NavLink>
+      <CardDiv>
+        <div className="flex flex-col-reverse lg:flex-row justify-around gap-5">
+          <div className="flex flex-col gap-2 items-center justify-center">
+            <h1 className="text-4xl font-bold">{t("intro-title")}</h1>
+            <p className="text-lg text-center">{t("intro-text")}</p>
+            <div className="flex flex-row gap-4">
+              <NavLink href="projects">
+                <ButtonColorOut>{t("intro-projects")}</ButtonColorOut>
+              </NavLink>
+              <NavLink href="contact">
+                <ButtonColorOut>{t("intro-contact")}</ButtonColorOut>
+              </NavLink>
+            </div>
+          </div>
+          <div className="flex flex-col items-center justify-center">
+            <Image src="/assets/logos/dflogo.svg" alt="Digital Forge Logo" width={400} height={400} />
           </div>
         </div>
-        <div className="flex flex-col items-center justify-center">
-          <Image src="/assets/logos/dflogo.svg" alt="Digital Forge Logo" width={400} height={400} />
-        </div>
-      </div>
-      <div className={`w-full flex flex-row ${maxWidth} justify-around gap-5 bg-white/90 backdrop-blur-md p-4 rounded-md`}>
+      </CardDiv>
+      <CardDiv>
         <div className="flex flex-col gap-2 items-center justify-center">
           <h1 className="text-4xl font-bold">{t("who-are-we-title")}</h1>
           <p className="text-lg text-center">{t("who-are-we-text")}</p>
         </div>
-      </div>
-      <div className={`w-full flex flex-row ${maxWidth} justify-around gap-5 bg-white/90 backdrop-blur-md p-4 rounded-md`}>
+      </CardDiv>
+      <CardDiv>
         <div className="flex flex-col gap-6 items-center justify-center">
           <h1 className="text-4xl font-bold">{t("services-title")}</h1>
-          <div className="grid grid-cols-3 gap-12 w-full">
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-12 w-full">
             <div className="flex flex-col gap-2 items-center">
               <FaChartLine color="yellow" className="bg-black p-4 rounded-md" size={64} />
               <h1 className="text-2xl font-bold">{t("services-1-title")}</h1>
@@ -60,21 +67,23 @@ export default function Home({ params: { locale } }: Props) {
             </div>
           </div>
         </div>
-      </div>
-      <div className={`w-full flex flex-col ${maxWidth} justify-around gap-5 bg-white/90 backdrop-blur-md p-4 rounded-md`}>
-        <h1 className="text-4xl font-bold">{t("why-us-title")}</h1>
-        <div className="flex flex-col gap-2 items-start justify-center">
-          <h1 className="text-2xl font-bold">{t("why-us-1-title")}</h1>
-          <p className="text-justify">{t("why-us-1-text")}</p>
-          <h1 className="text-2xl font-bold">{t("why-us-2-title")}</h1>
-          <p className="text-justify">{t("why-us-2-text")}</p>
-          <h1 className="text-2xl font-bold">{t("why-us-3-title")}</h1>
-          <p className="text-justify">{t("why-us-3-text")}</p>
-          <h1 className="text-2xl font-bold">{t("why-us-4-title")}</h1>
-          <p className="text-justify">{t("why-us-4-text")}</p>
-          <p className="text-justify font-bold">{t("why-us-5-text")}</p>
+      </CardDiv>
+      <CardDiv>
+        <div className="flex flex-col gap-6 items-center justify-center">
+          <h1 className="text-4xl font-bold">{t("why-us-title")}</h1>
+          <div className="flex flex-col gap-2 items-start justify-center">
+            <h1 className="text-2xl font-bold">{t("why-us-1-title")}</h1>
+            <p className="text-justify">{t("why-us-1-text")}</p>
+            <h1 className="text-2xl font-bold">{t("why-us-2-title")}</h1>
+            <p className="text-justify">{t("why-us-2-text")}</p>
+            <h1 className="text-2xl font-bold">{t("why-us-3-title")}</h1>
+            <p className="text-justify">{t("why-us-3-text")}</p>
+            <h1 className="text-2xl font-bold">{t("why-us-4-title")}</h1>
+            <p className="text-justify">{t("why-us-4-text")}</p>
+            <p className="text-justify font-bold">{t("why-us-5-text")}</p>
+          </div>
         </div>
-      </div>
+      </CardDiv>
       <AnimatedBackground />
     </div>
   );
--- src/app/[locale]/services/page.tsx
+++ src/app/[locale]/services/page.tsx
@@ -1,32 +1,37 @@
-import { ButtonColorOut } from "@/components/ButtonColorOut";
-import { NavLink } from "@/components/navigation/NavLink";
 import { useTranslations } from "next-intl";
 import { unstable_setRequestLocale } from "next-intl/server";
-import Image from "next/image";
 
 type Props = {
   params: { locale: string };
+};
+
+const Title = ({ text }: { text: string }) => {
+  return <h2 className="text-2xl font-semibold">{text}</h2>;
+};
+
+const Text = ({ text }: { text: string }) => {
+  return <p className="pl-4 text-justify font-medium">{text}</p>;
 };
 
 export default function Services({ params: { locale } }: Props) {
   unstable_setRequestLocale(locale);
   const t = useTranslations();
   return (
-    <div className="w-full flex flex-row justify-around gap-5 p-12">
-      <div className="flex flex-col gap-2 items-center justify-center">
-        <h1 className="text-4xl font-bold">{t("intro-title")}</h1>
-        <p className="text-lg text-center">{t("intro-text")}</p>
-        <div className="flex flex-row gap-4">
-          <NavLink href="projects">
-            <ButtonColorOut>{t("intro-projects")}</ButtonColorOut>
-          </NavLink>
-          <NavLink href="contact">
-            <ButtonColorOut>{t("intro-contact")}</ButtonColorOut>
-          </NavLink>
-        </div>
-      </div>
-      <div className="flex flex-col items-center justify-center">
-        <Image src="/assets/logos/dflogo.svg" alt="Digital Forge Logo" width={400} height={400} />
+    <div className="flex flex-col w-full items-center p-8">
+      <div className="flex flex-col max-w-[1500px] gap-6 items-start justify-center">
+        <h1 className="text-5xl font-bold mx-auto">{t("services-title")}</h1>
+        <Title text={t("service-1-title")} />
+        <Text text={t("service-1-text")} />
+        <Title text={t("service-2-title")} />
+        <Text text={t("service-2-text")} />
+        <Title text={t("service-3-title")} />
+        <Text text={t("service-3-text")} />
+        <Title text={t("service-4-title")} />
+        <Text text={t("service-4-text")} />
+        <Title text={t("service-5-title")} />
+        <Text text={t("service-5-text")} />
+        <Title text={t("service-6-title")} />
+        <Text text={t("service-6-text")} />
       </div>
     </div>
   );
