e3306b38228968c215e6b844f6c5d8c160859608
huseyinonalcom <65453275+huseyinonalcom@users.noreply.github.com>
2024-08-19 03:48:46 +0300

    Update not-found.tsx

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,8 @@
 import { NavLink } from "@/components/navigation/NavLink";
+import { unstable_setRequestLocale } from "next-intl/server";
 
 export default function NotFound() {
+  unstable_setRequestLocale("en");
   return (
     <html lang="en">
       <body className="flex flex-col items-center justify-center min-h-[90vh]">
