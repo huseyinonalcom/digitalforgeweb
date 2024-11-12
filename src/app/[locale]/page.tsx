2df68fbafccc33bdb5ec4ff7645c5a46ce9e346a
Huseyin Onal <65453275+huseyinonalcom@users.noreply.github.com>
2024-11-12 14:28:01 +0300

    s

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -7,6 +7,7 @@ import Image from "next/image";
 import { FaChartLine, FaCode, FaPaintBrush } from "react-icons/fa";
 import dflogo from "../../../public/assets/logos/dflogo.svg";
 import imageAdvertisement from "../../../public/assets/images/advertisement.jpg";
+import Contact from "./contact/page";
 
 export default function Home() {
   const t = useTranslations();
@@ -33,7 +34,6 @@ export default function Home() {
           </div>
         </div>
       </BoxDiv>
-
       <BoxDiv className="bg-white">
         <div className="flex flex-col gap-6 items-center justify-center max-w-[1500px] mx-auto">
           <h1 className="text-4xl font-bold">{t("who-are-we-title")}</h1>
@@ -107,34 +107,11 @@ export default function Home() {
       </BoxDiv>
       <BoxDiv className="bg-white">
         <div className="flex flex-col gap-6 items-center justify-center max-w-[1500px] mx-auto">
-          <h1 className="text-4xl font-bold">{t("why-us-title")}</h1>
-          <div className="grid grid-cols-2 lg:grid-cols-4 gap-12 w-full">
-            <div className="flex flex-col gap-2 items-start rounded-md p-4 border-2 border-black min-h-52">
-              <div className="flex flex-row items-center justify-between w-full">
-                <h1 className="text-xl font-bold">{t("why-us-1-title")}</h1>
-              </div>
-              <p className="text-justify">{t("why-us-1-text")}</p>
-            </div>
-            <div className="flex flex-col gap-2 items-start rounded-md p-4 border-2 border-black min-h-52">
-              <div className="flex flex-row items-center justify-between w-full">
-                <h1 className="text-xl font-bold">{t("why-us-2-title")}</h1>
-              </div>
-              <p className="text-justify">{t("why-us-2-text")}</p>
-            </div>
-            <div className="flex flex-col gap-2 items-start rounded-md p-4 border-2 border-black min-h-52">
-              <div className="flex flex-row items-center justify-between w-full">
-                <h1 className="text-xl font-bold">{t("why-us-3-title")}</h1>
-              </div>
-              <p className="text-justify">{t("why-us-3-text")}</p>
-            </div>
-            <div className="flex flex-col gap-2 items-start rounded-md p-4 border-2 border-black min-h-52">
-              <div className="flex flex-row items-center justify-between w-full">
-                <h1 className="text-xl font-bold">{t("why-us-4-title")}</h1>
-              </div>
-              <p className="text-justify">{t("why-us-4-text")}</p>
-            </div>
-          </div>
+          <h1 className="text-4xl font-bold">{t("our-partners")}</h1>
         </div>
+      </BoxDiv>
+      <BoxDiv className="bg-white">
+        <Contact />
       </BoxDiv>
     </div>
   );
