32c9de45bf22a8876b3ac668f0b1d75b832aa341
huseyinonalcom <65453275+huseyinonalcom@users.noreply.github.com>
2024-11-18 23:35:13 +0300

    d

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,12 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin();
 
-const nextConfig: NextConfig = withNextIntl({});
+const nextConfig: NextConfig = withNextIntl({
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+});
 
 export default nextConfig;
