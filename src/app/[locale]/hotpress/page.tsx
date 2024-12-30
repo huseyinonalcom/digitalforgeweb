6766d0401ebff88256ea15e783bdbdb6501981e7
Huseyin Onal <65453275+huseyinonalcom@users.noreply.github.com>
2024-12-30 17:18:56 +0300

    Delete page.tsx

--- src/app/[locale]/hotpress/page.tsx
+++
@@ -1,9 +0,0 @@
-import Image from "next/image";
-
-export default function HotPress() {
-  return (
-    <div className="w-full flex flex-col gap-5 items-center p-12">
-      <Image priority src="/assets/logos/hotpress.svg" alt="Digital Forge Logo" width={200} height={200} />
-    </div>
-  );
-}
